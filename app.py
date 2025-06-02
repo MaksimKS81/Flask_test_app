f971832bcd36cefc6b4d700ff4d69ec19e46b321
Maksim Krivolapov <mkrivolapov@darimotion.com>
2025-06-02 15:43:16 +0200

    changed from flex to standart

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def hello() -> str:
     Returns:
         A string with the words 'Hello World!'.
     """
-    return "Hello Flask! Trigger test 4"
+    return "Hello Flask! Trigger test 5"
 
 @app.route("/test")
 def test() ->str:
